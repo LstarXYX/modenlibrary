00059b28a187b86ec384c3971cddf754d1eb11d5
LstarXYX <294554846@qq.com>
2020-12-26 22:48:32 +0800

    增加了消息推送接口和服务

---
+++ src/main/java/modenlibrary/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package modenlibrary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocket 配置
+ * @author L.star
+ * @date 2020/12/26 21:42
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
--- src/main/java/modenlibrary/controller/IndexController.java
+++ src/main/java/modenlibrary/controller/IndexController.java
@@ -1,7 +1,15 @@
 package modenlibrary.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import modenlibrary.Common.code.ReturnCode;
+import modenlibrary.Common.socket.WebSocketServer;
+import modenlibrary.Common.utils.Result;
+import modenlibrary.Common.vo.ResultVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 /**
  * 跳转界面
@@ -9,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @date 2020/12/23 15:52
  */
 @Controller
+@Slf4j
 public class IndexController {
 
     @GetMapping({"/","/index"})
@@ -25,4 +34,25 @@ public class IndexController {
     public String upload(){
         return "fileupload";
     }
+
+    /**
+     * 管理员群发消息
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/im/sendall")
+    @ResponseBody
+    public ResultVo sendAllMsg(String message){
+        try {
+            WebSocketServer.BroadCastInfo(message);
+        }catch (IOException e){
+            log.error("群发消息失败");
+            return Result.fail(ReturnCode.SYSTEM_ERROR);
+        }
+        return Result.success("ok");
+    }
+
+
+
 }
